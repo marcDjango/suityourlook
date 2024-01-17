26af4b0a166e3376b037b75bb4af01fc63834495
Quentin Vayssieres <quentin.vayssieres@gmail.com>
2024-01-17 18:32:49 +0100

    feat: design card

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+// eslint-disable-next-line no-unused-vars
+import { useEffect, useState } from "react";
 import "./Card.scss";
 import model1 from "../../assets/images/model1.jpg";
 
 function Card() {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchModelsProducts = async () => {
+  //     try {
+  //       const response = await fetch("/api/models_products");
+  //       if (!response.ok) {
+  //         throw new Error("Erreur réseau");
+  //       }
+
+  //       const data = await response.json();
+  //       setData(data);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   fetchModelsProducts();
+  // }, []);
+
   return (
     <div className="modal">
       <div className="main-card">
@@ -10,11 +31,21 @@ function Card() {
           <img src={model1} alt="prise" />
         </div>
         <div className="text-main-card">
-          <div className="make-up">Maquillage :</div>
-          <div className="skin-care">Soins de la peau :</div>
-          <div className="hair-care">Soins des cheveux :</div>
-          <div className="shampoo">Shampooing :</div>
-          <div className="fragrance">Parfum :</div>
+          <div className="make-up">
+            <h3>Maquillage</h3>
+          </div>
+          <div className="skin-care">
+            <h3>Soins de la peau</h3>
+          </div>
+          <div className="hair-care">
+            <h3>Soins des cheveux</h3>
+          </div>
+          <div className="shampoo">
+            <h3>Shampooing</h3>
+          </div>
+          <div className="fragrance">
+            <h3>Parfum</h3>
+          </div>
         </div>
       </div>
     </div>
