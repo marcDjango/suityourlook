16f836b1a181158352a6cee2d4966abb8c0d2235
Marcelo <marceloromero2016ar@gmail.com>
2024-01-18 12:32:55 +0100

    feat: finish style modal

--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/prop-types */
+
+import React, { useEffect, useRef } from "react";
 import "./modal.scss";
 
-function Modal() {
+function Modal({ isModal, setIsModal, dataCategories }) {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    if (isModal && modalRef.current) {
+      modalRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [isModal, dataCategories]);
+
   return (
-    <div className="contain-modal">
-      <div className="body-modal" id="view-modal">
-        <div className="header-body-modal">close</div>
+    <div className="contain-modal" onClick={() => setIsModal(!isModal)}>
+      <div
+        ref={modalRef}
+        className="body-modal"
+        id="view-modal"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
         <div className="main-body-modal">
-          <div className="Card" />
-          <div className="Card" />
-          <div className="Card" />
-          <div className="Card" />
-          <div className="Card" />
-          <div className="Card" />
-          <div className="Card" />
+          {dataCategories.map((element) => (
+            <div key={element.id} className="Card">
+              <img src={element.image} alt="" />
+            </div>
+          ))}
         </div>
       </div>
     </div>
