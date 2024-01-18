7bf90d67e1dadccde7f49fb8be59d95db5f668dd
Marcelo <marceloromero2016ar@gmail.com>
2024-01-18 20:39:53 +0100

    chore: pull dev

--- frontend/src/pages/ProfilUser/profiluser.jsx
+++ frontend/src/pages/ProfilUser/profiluser.jsx
@@ -7,11 +7,83 @@ import "./profiluser.scss";
 function Profiluser() {
   const [isChangeProfil, setIsChangeProfil] = useState(true);
   const [isShowProfil, setIsShowProfil] = useState(false);
+
+  const UpdateProfil = async ({ request }) => {
+    // Récupération des données du formulaire depuis la requête
+
+    try {
+      const form = await request.formData();
+      if (isShowProfil) {
+        const formData = Object.fromEntries(form);
+        // Envoi d'une requête POST au backend pour l'authentification
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/users/:id`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        // Vérification de la réponse HTTP
+        if (!response.ok) {
+          // En cas d'échec, une erreur est levée avec un message explicatif
+          throw new Error(
+            "Le mot de passe ou l'adresse e-mail que vous avez entré est incorrect. Veuillez vérifier vos informations et réessayer. Assurez-vous que votre adresse e-mail est correctement saisie et que le mot de passe respecte les critères requis."
+          );
+        }
+
+        // Récupération des données JSON de la réponse
+        const data = await response.json();
+
+        // Stockage des informations de l'utilisateur et du token dans le localStorage
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+      } else {
+        const data = Object.fromEntries(form);
+
+        const formData = {
+          ...data,
+          is_admin: 0,
+        };
+        if (formData.genre === "Mr") {
+          formData.genre = 1;
+        } else {
+          formData.genre = 0;
+        }
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/users`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+        if (data.password !== data.passwordconfirme) {
+          throw new Error(
+            "Le mot de passe ou l'adresse e-mail que vous avez entré est incorrect. Veuillez vérifier vos informations et réessayer. Assurez-vous que votre adresse e-mail est correctement saisie et que le mot de passe respecte les critères requis."
+          );
+        }
+
+        if (!response.ok) {
+          throw new Error("cant fetch user");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    // Redirection de l'utilisateur vers la page d'accueil
+  };
+
   return (
     <div className="contain-profilpage">
       <div className="contain-profilpage-body">
         <div className="contain-profilpage-header">
-          Home `{">"}` Mon Compte `{">"}` Information Du Compte{" "}
+          Home {">"} Mon Compte {">"} Information Du Compte
         </div>
 
         <div className="contain-profilpage-main">
@@ -23,22 +95,22 @@ function Profiluser() {
               <div className="side-links">
                 {" "}
                 <button className="btn-link">MON COMPTE</button>
-                <p>`&gt;`</p>
+                <p>&gt;</p>
               </div>
               <div className="side-links">
                 {" "}
                 <button className="btn-link">HISTORIQUE DES COMMANDES</button>
-                <p>`&gt;`</p>
+                <p>&gt;</p>
               </div>
               <div className="side-links">
                 {" "}
                 <button className="btn-link">CARNET D'ADRESSES</button>
-                <p>`&gt;`</p>
+                <p>&gt;</p>
               </div>
               <div className="side-links">
                 {" "}
                 <button className="btn-link">LISTE DES FAVORIS</button>
-                <p>`&gt;`</p>
+                <p>&gt;</p>
               </div>
             </div>
             <div className="side-button">
@@ -57,34 +129,36 @@ function Profiluser() {
               {isShowProfil && (
                 <>
                   {" "}
-                  <div className="main-contain-action">
-                    <h4>MON COMPTE</h4>
-                    <div className="main-contain-button">
-                      <button type="button" className="btn-action">
-                        MODIFIER LES INSFORMATIONS
-                      </button>
-                      <button type="button" className="btn-action">
-                        CHANGER LE MOT DE PASSE
-                      </button>
+                  <form>
+                    <div className="main-contain-action">
+                      <h4>MON COMPTE</h4>
+                      <div className="main-contain-button">
+                        <button type="button" className="btn-action">
+                          MODIFIER LES INFORMATIONS
+                        </button>
+                        <button type="button" className="btn-action">
+                          CHANGER LE MOT DE PASSE
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                  <div className="main-contain-form-login">
-                    <div className="row-form-login">
-                      <label htmlFor="" className="label-form-input">
-                        Nom
-                      </label>
-                      <input type="text" placeholder="Romero Marcelo" />
+                    <div className="main-contain-form-login">
+                      <div className="row-form-login">
+                        <label htmlFor="" className="label-form-input">
+                          Nom
+                        </label>
+                        <input type="text" placeholder="Romero Marcelo" />
+                      </div>
+                      <div className="row-form-login">
+                        <label htmlFor="" className="label-form-input">
+                          Adresse email
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="marceloromero2016ar@gmail.com"
+                        />
+                      </div>
                     </div>
-                    <div className="row-form-login">
-                      <label htmlFor="" className="label-form-input">
-                        Adresse email
-                      </label>
-                      <input
-                        type="text"
-                        placeholder="marceloromero2016ar@gmail.com"
-                      />
-                    </div>
-                  </div>
+                  </form>
                 </>
               )}
 
@@ -94,6 +168,13 @@ function Profiluser() {
                     <h4>MODIFIER LES INFORMATIONS</h4>
                   </div>
                   <div className="main-contain-form-login">
+                    <label className="label" htmlFor="genre">
+                      Genre
+                      <select className="select" name="genre" id="genre">
+                        <option>Mr</option>
+                        <option>Mme</option>
+                      </select>
+                    </label>
                     <div className="row-form-login">
                       <label htmlFor="" className="label-form-input">
                         Prenom*
